c5d3d2f07f9f5c021075e8bbe3a32f18cbca12a3
ev.v.kiselev <ev.v.kiselev@tinkoff.ru>
2022-09-15 16:37:18 +0700

    Добавил тест для square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/test_program.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/test_program.java
@@ -4,15 +4,14 @@ public class test_program {
   public static void main(String[] args) {
     hello("kek");
     hello("lol");
-    System.out.println(area(6));
+    Square s = new Square(5);
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь квадрата со стороной " + s.l + "  = " + s.area());
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody);
-  }
-
-  public static double area(double l) {
-    return l * l;
   }
 
 }
