0cf5de5b84c84b30a569e5fa52a457bf1fc27882
ev.v.kiselev <ev.v.kiselev@tinkoff.ru>
2022-09-15 11:06:37 +0700

    Добавлено несколько новых функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/test_program.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/test_program.java
@@ -2,7 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class test_program {
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    hello("kek");
+    hello("lol");
+    System.out.println(area(6));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody);
+  }
+
+  public static double area(double l) {
+    return l * l;
   }
 
 }
